80301b61e235071baf62329a3d092a856103a2d5
Nicholas Bauer <nicholasbauer@outlook.com>
2019-11-05 10:39:27 -0500

    Added hopefully more efficient and accurate exclusivenearestneighbors

--- src/analysis/association.jl
+++ src/analysis/association.jl
@@ -7,21 +7,65 @@ Finds the closest of all pairs, removes both, and iterates on the remainder unti
 
 NOTE: Assumes that your data is far away from the type's maximum value.
 """
-function exclusivenearestneighbors(points1::Vector{T}, points2::Vector{T}) where T <: DataEntity
+function exclusivenearestneighbors1(points1::Vector{T}, points2::Vector{T}) where T <: DataEntity
     points2larger = length(points2) > length(points1)
 
     (points1, points2) = points2larger ?
-        (points2, points1) :
-        (points1, points2)
+        (points2, copy(points1)) :
+        (points1, copy(points2))
 
     coordinates1 = extractcoordinates(points1)
-
     coordinates1 = ndims(coordinates1) == 1 ?
         make2d(coordinates1) :
         coordinates1
 
-    neighbors = T[]
+    neighbors1 = T[]
+    neighbors2 = T[]
     distances = eltype(coordinates1)[]
+
+    neighbortree = BallTree(coordinates1)
+    coordinates1used = falses(length(points1))
+
+    while !isempty(points2)
+        coordinates2 = extractcoordinates(points2)
+        coordinates2used = falses(length(points2))
+        inearestneighbors, nearestneighbor_distances = nn(neighbortree, coordinates2, i -> coordinates1used[i])
+        while any(nearestneighbor_distances .< typemax(eltype(coordinates1)))
+            mindist, mini = findmin(nearestneighbor_distances)
+            nearestneighbor_distances[mini] = typemax(eltype(coordinates1))
+            coordinates2used[mini] && continue
+            inearestneighbor = inearestneighbors[mini]
+            coordinates1used[inearestneighbor] && continue
+            coordinates2used[mini] = coordinates1used[inearestneighbor] = true
+            push!(neighbors1, points1[inearestneighbor])
+            push!(neighbors2, points2[mini])
+            push!(distances, mindist)
+        end
+        deleteat!(points2, coordinates2used)
+    end
+
+    if points2larger
+        neighbors2, neighbors1, distances
+    else
+        neighbors1, neighbors2, distances
+    end
+end
+
+function exclusivenearestneighbors(points1::Vector{T}, points2::Vector{T}) where T <: DataEntity
+    points2larger = length(points2) > length(points1)
+
+    if points2larger
+        points1, points2 = points2, points1
+    end
+
+    coordinates1 = extractcoordinates(points1)
+
+    if ndims(coordinates1) == 1
+        coordinates1 = make2d(coordinates1) # ensures array is 2d, currently needed for Tree
+    end
+
+    neighbors = Vector{T}()
+    distances = Vector{eltype(coordinates1)}()
 
     for point ∈ points2
         coordinates = extractcoordinates(point)
@@ -70,7 +114,7 @@ function montecarloaffinity(molecules1::Vector{T}, molecules2::Vector{T}, ch1_ne
     percentileranks = pmap(localmontecarlo, nlocalmolecules1, nlocalmolecules2, distances, (localradius for i = 1:length(ch1_neighbors)), (10000 for i = 1:length(ch1_neighbors)), on_error = identity)
 
     return percentileranks
-end
+end # since number might be finite, could potentially generate say 100,000 permutations once and use for all with same counts...
 
 """
     localmontecarlo()
