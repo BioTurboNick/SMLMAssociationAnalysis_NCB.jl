70ec9a2967a9531bd2b1f33414bbfc0da47bd1d6
Nicholas Bauer <nicholasbauer@outlook.com>
2019-11-05 10:31:52 -0500

    Added variants just for optimizing parameters

--- src/data/ChannelData.jl
+++ src/data/ChannelData.jl
@@ -8,3 +8,14 @@ struct ChannelData
     molecules::Vector{<:DataEntity}
     neighbormolecules::Vector{<:DataEntity}
 end
+
+"""
+    ChannelDataOptimizing
+
+Stores the results from many molecule lists. Intended for parameter optimizing only.
+"""
+struct ChannelDataOptimizing
+    channelname::AbstractString
+    molecules::Vector{Vector{<:DataEntity}}
+    neighbormolecules::Vector{<:DataEntity}
+end
--- src/data/Result.jl
+++ src/data/Result.jl
@@ -14,3 +14,21 @@ struct Result
     mediandistance::AbstractFloat
     percentileranks::Vector{Float64}
 end
+
+"""
+    ResultOptimizing
+
+Stores the results from an experiment with extra data for optimization purposes.
+"""
+struct ResultOptimizing
+    projectdirname::AbstractString
+    experimentdirname::AbstractString
+    replicate::Int
+    samplename::AbstractString
+    cell::Int
+    channels::Vector{ChannelDataOptimizing}
+    distances::Vector{AbstractFloat}
+    mediandistance::AbstractFloat
+    percentileranks::Vector{Float64}
+    percentileranks_by_localdensity::Vector{Vector{Float64}}
+end
