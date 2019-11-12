3277676e589c4d862cca707e8ef4a57df6001770
Nicholas Bauer <nicholasbauer@outlook.com>
2019-11-12 00:04:01 -0500

    Fixed graphs for publication

--- src/paper/run_optimize_analysis.jl
+++ src/paper/run_optimize_analysis.jl
@@ -32,11 +32,11 @@ average_percentileranks = [[[[[mean(x) for x ∈ experimentresults[l][k][j][i].p
                                        for k ∈ eachindex(experimentresults[l])]
                                        for l ∈ eachindex(experimentresults)]
 
-average_percentileranks = [[average_percentileranks[l][k][j][i][m] for i in 1:10
-                                                                   for j in 1:4
-                                                                   for k in 1:3
-                                                                   for l in 1:2]
-                                                                   for m in 1:10]
+average_percentileranks1 = [[average_percentileranks[l][k][j][i][m] for i in 1:10
+                                                                    for j in 1:4
+                                                                    for k in 1:3
+                                                                    for l in 1:2]
+                                                                    for m in 1:10]
 
 mean_average_percentileranks = mean.(average_percentileranks1)
 sem_average_percentileranks = sem.(average_percentileranks1)
@@ -45,7 +45,7 @@ ci95_average_percentileranks = 1.96 .* sem_average_percentileranks
 xvals = [200, 400, 600, 800, 1000, 1200, 1400, 1600, 1800, 2000]
 import Plots.mm
 plot(xvals, average_percentileranks, line=(0.25))
-plot!(xvals, mean_average_percentileranks, line=(stroke(10), :black), yaxis=(0:1), xaxis=(0:400:2000), tickfontsize=72, legend=:none, size=(1024,2048), left_margin=15mm, top_margin=5mm, right_margin=3mm, bottom_margin=5mm, ywiden=false)
+plot!(xvals, mean_average_percentileranks, line=(stroke(10), :black), yaxis=(0:1), xaxis=(0:400:2000), tickfontsize=96, legend=:none, size=(1024,2048), left_margin=20mm, top_margin=5mm, right_margin=10mm, bottom_margin=6mm, ywiden=false)
 plot!(xvals, mean_average_percentileranks - ci95_average_percentileranks, line=(stroke(10), :gray))
 plot!(xvals, mean_average_percentileranks + ci95_average_percentileranks, line=(stroke(10), :gray))
 
@@ -78,7 +78,7 @@ ci95_ch1_frac = 1.96 .* sem_ch1_frac
 xvals = [0, 10, 20, 50, 100, 200, 500, 1000, 2000, 5000]
 
 plot(xvals, ch1_frac, line=(0.25))
-plot!(xvals, mean_ch1_frac, line=(stroke(10), :black), yaxis=(0:1), xaxis=(0:1000:5000), tickfontsize=64, guidefontsize=72, legend=:none, size=(1024,2048), left_margin=15mm, top_margin=5mm, right_margin=5mm, bottom_margin=5mm, ywiden=false)
+plot!(xvals, mean_ch1_frac, line=(stroke(10), :black), yaxis=(0:1), xaxis=(0:1000:5000), tickfontsize=96, legend=:none, size=(1024,2048), left_margin=17mm, top_margin=5mm, right_margin=10mm, bottom_margin=4mm, ywiden=false)
 plot!(xvals, mean_ch1_frac + ci95_ch1_frac, line=(stroke(10), :gray))
 plot!(xvals, mean_ch1_frac - ci95_ch1_frac, line=(stroke(10), :gray))
 
@@ -110,7 +110,7 @@ ci95_ch2_frac = 1.96 .* sem_ch2_frac
 xvals = [0, 10, 20, 50, 100, 200, 500, 1000, 2000, 5000]
 
 plot(xvals, ch2_frac, line=(0.25))
-plot!(xvals, mean_ch2_frac, line=(stroke(10), :black), yaxis=(0:1), xaxis=(0:1000:5000), tickfontsize=64, guidefontsize=72, legend=:none, size=(1024,2048), left_margin=15mm, top_margin=5mm, bottom_margin=3mm, right_margin=5mm, ywiden=false)
+plot!(xvals, mean_ch2_frac, line=(stroke(10), :black), yaxis=(0:1), xaxis=(0:1000:5000), tickfontsize=96, legend=:none, size=(1024,2048), left_margin=17mm, top_margin=5mm, bottom_margin=4mm, right_margin=10mm, ywiden=false)
 plot!(xvals, mean_ch2_frac + ci95_ch2_frac, line=(stroke(10), :gray))
 plot!(xvals, mean_ch2_frac - ci95_ch2_frac, line=(stroke(10), :gray))
 
