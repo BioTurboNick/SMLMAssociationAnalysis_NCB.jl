e7bb7cae6e2edc7bf248b6ee0b5e316466fb1611
Nicholas Bauer <nicholasbauer@outlook.com>
2019-11-11 23:43:14 -0500

    Refined plots for paper

--- src/paper/run_optimize_analysis.jl
+++ src/paper/run_optimize_analysis.jl
@@ -20,81 +20,98 @@ datapath = joinpath(outputdir, "results_optimize.jld2")
 
 experimentresults = FileIO.load(datapath)["experimentresults"]
 
-medianmeasurements = Array{Float64,4}(undef, ncells, nreplicates, 4, 2)
-montecarlomeasurements = Array{Float64,4}(undef, ncells, nreplicates, 4, 2)
-localization1counts = Array{Float64,4}(undef, ncells, nreplicates, 4, 2)
-molecule1counts = Array{Float64,4}(undef, ncells, nreplicates, 4, 2)
-localization2counts = Array{Float64,4}(undef, ncells, nreplicates, 4, 2)
-molecule2counts = Array{Float64,4}(undef, ncells, nreplicates, 4, 2)
-
-for k ∈ 1:2
-    for i ∈ 1:nsamples
-        samplemedianresults = Array{Float64,2}(undef, ncells, nreplicates)
-        samplelessthan10results = Array{Float64,2}(undef, ncells, nreplicates)
-        samplelocalization1counts = Array{Float64,2}(undef, ncells, nreplicates)
-        samplemolecule1counts = Array{Float64,2}(undef, ncells, nreplicates)
-        samplelocalization2counts = Array{Float64,2}(undef, ncells, nreplicates)
-        samplemolecule2counts = Array{Float64,2}(undef, ncells, nreplicates)
-        for j ∈ 1:nreplicates
-            replicateresults = experimentresults[k][j][i]
-            lessthanlimitreplicate = [(x.distances .< 200) .& (x.percentileranks .< 0.1) for x ∈ replicateresults]
-            lessthan10 = count.(lessthanlimitreplicate) ./ length.(lessthanlimitreplicate)
-            mediandistances = map(x -> x.mediandistance, replicateresults)
-            samplemedianresults[:, j] = mediandistances
-            samplelessthan10results[:, j] = lessthan10
-            samplelocalization1counts[:, j] = [map(x -> x.group.localizations |> length, y.channels[1].molecules) |> sum for y ∈ replicateresults]
-            samplelocalization2counts[:, j] = [map(x -> x.group.localizations |> length, y.channels[2].molecules) |> sum for y ∈ replicateresults]
-            samplemolecule1counts[:, j] = map(x -> x.channels[1].molecules |> length, replicateresults)
-            samplemolecule2counts[:, j] = map(x -> x.channels[2].molecules |> length, replicateresults)
-        end
-        medianmeasurements[:, :, i, k] = samplemedianresults
-        montecarlomeasurements[:, :, i, k] = samplelessthan10results
-        localization1counts[:, :, i, k] = samplelocalization1counts
-        molecule1counts[:, :, i, k] = samplemolecule1counts
-        localization2counts[:, :, i, k] = samplelocalization2counts
-        molecule2counts[:, :, i, k] = samplemolecule2counts
-    end
-end
-
 
 ### Following optimization requires running run_optimize.jl instead of run_original.jl
+using Plots
+using StatsBase
 
 # robustness (local distance)
-average_percentileranks = [[[mean(x) for x ∈ experimentresults[1][k][j][i].percentileranks_by_localdensity]
-                                     for i ∈ eachindex(experimentresults[1][k][j])]
-                                     for j ∈ eachindex(experimentresults[1][k])]
-                                     for k ∈ eachindex(experimentresults[1])
+average_percentileranks = [[[[[mean(x) for x ∈ experimentresults[l][k][j][i].percentileranks_by_localdensity]
+                                       for i ∈ eachindex(experimentresults[l][k][j])]
+                                       for j ∈ eachindex(experimentresults[l][k])]
+                                       for k ∈ eachindex(experimentresults[l])]
+                                       for l ∈ eachindex(experimentresults)]
 
-plot([200, 400, 600, 800, 1000, 1200, 1400, 1600, 1800, 2000], average_percentileranks)
+average_percentileranks = [[average_percentileranks[l][k][j][i][m] for i in 1:10
+                                                                   for j in 1:4
+                                                                   for k in 1:3
+                                                                   for l in 1:2]
+                                                                   for m in 1:10]
 
-savefig(joinpath(outputdir, "(partial) choice of local area effect on percentile rank.png"))
+mean_average_percentileranks = mean.(average_percentileranks1)
+sem_average_percentileranks = sem.(average_percentileranks1)
+ci95_average_percentileranks = 1.96 .* sem_average_percentileranks
+
+xvals = [200, 400, 600, 800, 1000, 1200, 1400, 1600, 1800, 2000]
+import Plots.mm
+plot(xvals, average_percentileranks, line=(0.25))
+plot!(xvals, mean_average_percentileranks, line=(stroke(10), :black), yaxis=(0:1), xaxis=(0:400:2000), tickfontsize=72, legend=:none, size=(1024,2048), left_margin=15mm, top_margin=5mm, right_margin=3mm, bottom_margin=5mm, ywiden=false)
+plot!(xvals, mean_average_percentileranks - ci95_average_percentileranks, line=(stroke(10), :gray))
+plot!(xvals, mean_average_percentileranks + ci95_average_percentileranks, line=(stroke(10), :gray))
+
+savefig(joinpath(outputdir, "choice of local area effect on percentile rank.png"))
 
 # temporal cutoff (needs special data)
 
-ch1_counts = [[[[length(x) for x ∈ experimentresults[1][k][j][i].channels[1].molecules]
-                           for i ∈ eachindex(experimentresults[1][k][j])]
-                           for j ∈ eachindex(experimentresults[1][k])]
-                           for k ∈ eachindex(experimentresults[1])]
+ch1_counts = [[[[[length(x) for x ∈ experimentresults[l][k][j][i].channels[1].molecules]
+                            for i ∈ eachindex(experimentresults[l][k][j])]
+                            for j ∈ eachindex(experimentresults[l][k])]
+                            for k ∈ eachindex(experimentresults[l])]
+                            for l ∈ eachindex(experimentresults)]
 
-ch1_frac = [[[[x ./ ch1_counts[k][j][i][1] for x ∈ ch1_counts[k][j][i]]
-                                       for i ∈ eachindex(experimentresults[1][k][j])]
-                                       for j ∈ eachindex(experimentresults[1][k])]
-                                       for k ∈ eachindex(experimentresults[1])]
+ch1_frac = [[[[[x ./ ch1_counts[l][k][j][i][1] for x ∈ ch1_counts[l][k][j][i]]
+                                            for i ∈ eachindex(experimentresults[l][k][j])]
+                                            for j ∈ eachindex(experimentresults[l][k])]
+                                            for k ∈ eachindex(experimentresults[l])]
+                                            for l ∈ eachindex(experimentresults)]
 
-plot([0, 10, 20, 50, 100, 200, 500, 1000, 2000, 5000], ch1_frac)
+ch1_frac1 = [[ch1_frac[l][k][j][i][m] for i in 1:10
+                                       for j in 1:4
+                                       for k in 1:3
+                                       for l in 1:2]
+                                       for m in 1:10]
 
-savefig(joinpath(outputdir, "(partial) t_off selection molecules 1.png"))
+mean_ch1_frac = mean.(ch1_frac1)
+sem_ch1_frac = sem.(ch1_frac1)
+ci95_ch1_frac = 1.96 .* sem_ch1_frac
 
-ch2_counts = [[[[length(x) for x ∈ experimentresults[1][k][j][i].channels[2].molecules]
-                           for i ∈ eachindex(experimentresults[1][k][j])]
-                           for j ∈ eachindex(experimentresults[1][k])]
-                           for k ∈ eachindex(experimentresults[1])]
+xvals = [0, 10, 20, 50, 100, 200, 500, 1000, 2000, 5000]
 
-ch2_frac = [[[[x ./ ch2_counts[k][j][i][1] for x ∈ ch2_counts[k][j][i]]
-                            for i ∈ eachindex(experimentresults[1][k][j])]
-                            for j ∈ eachindex(experimentresults[1][k])]
-                            for k ∈ eachindex(experimentresults[1])]
+plot(xvals, ch1_frac, line=(0.25))
+plot!(xvals, mean_ch1_frac, line=(stroke(10), :black), yaxis=(0:1), xaxis=(0:1000:5000), tickfontsize=64, guidefontsize=72, legend=:none, size=(1024,2048), left_margin=15mm, top_margin=5mm, right_margin=5mm, bottom_margin=5mm, ywiden=false)
+plot!(xvals, mean_ch1_frac + ci95_ch1_frac, line=(stroke(10), :gray))
+plot!(xvals, mean_ch1_frac - ci95_ch1_frac, line=(stroke(10), :gray))
 
-plot([0, 10, 20, 50, 100, 200, 500, 1000, 2000, 5000], ch2_frac)
+savefig(joinpath(outputdir, "choice of t_off selection on number of molecules 1.png"))
 
-savefig(joinpath(outputdir, "(partial) t_off selection molecules 2.png"))
+ch2_counts = [[[[[length(x) for x ∈ experimentresults[l][k][j][i].channels[2].molecules]
+                            for i ∈ eachindex(experimentresults[l][k][j])]
+                            for j ∈ eachindex(experimentresults[l][k])]
+                            for k ∈ eachindex(experimentresults[l])]
+                            for l ∈ eachindex(experimentresults)]
+
+ch2_frac = [[[[[x ./ ch2_counts[l][k][j][i][1] for x ∈ ch2_counts[l][k][j][i]]
+                                                for i ∈ eachindex(experimentresults[l][k][j])]
+                                                for j ∈ eachindex(experimentresults[l][k])]
+                                                for k ∈ eachindex(experimentresults[l])]
+                                                for l ∈ eachindex(experimentresults)]
+
+ch2_frac1 = [[ch2_frac[l][k][j][i][m] for i in 1:10
+                                       for j in 1:4
+                                       for k in 1:3
+                                       for l in 1:2]
+                                       for m in 1:10]
+
+mean_ch2_frac = mean.(ch2_frac1)
+sem_ch2_frac = sem.(ch2_frac1)
+ci95_ch2_frac = 1.96 .* sem_ch2_frac
+
+
+xvals = [0, 10, 20, 50, 100, 200, 500, 1000, 2000, 5000]
+
+plot(xvals, ch2_frac, line=(0.25))
+plot!(xvals, mean_ch2_frac, line=(stroke(10), :black), yaxis=(0:1), xaxis=(0:1000:5000), tickfontsize=64, guidefontsize=72, legend=:none, size=(1024,2048), left_margin=15mm, top_margin=5mm, bottom_margin=3mm, right_margin=5mm, ywiden=false)
+plot!(xvals, mean_ch2_frac + ci95_ch2_frac, line=(stroke(10), :gray))
+plot!(xvals, mean_ch2_frac - ci95_ch2_frac, line=(stroke(10), :gray))
+
+savefig(joinpath(outputdir, "choice of t_off selection on number of molecules 2.png"))
--- src/paper/run_original_analysis.jl
+++ src/paper/run_original_analysis.jl
@@ -212,28 +212,35 @@ nutdoxgroups = repeat([2, 1, 4, 3], inner=30)
 p53_mdm2_median = [medianmeasurements[:,:,1,1,1] |> vec; medianmeasurements[:,:,2,1,1] |> vec; medianmeasurements[:,:,1,2,1] |> vec; medianmeasurements[:,:,2,2,1] |> vec]
 boxplot(nutdoxgroups, p53_mdm2_median, outliers=false,
         label=["- Dox", "+ Dox"],
-        legend=:none,
         guidefontsize=12,
-        tickfontsize=10,
-        legendfontsize=10,
+        tickfontsize=72,
+        legend=:none,
+        left_margin=20mm,
+        top_margin=5mm,
+        bottom_margin=5mm,
         seriescolor=[:white :lightgray],
-        line=(3, :gray, 1.0),
+        line=(6, 1.0),
+        size=(1024,2048),
         xaxis=("Condition", (1:4, ["-Dox -Nut", "+Dox -Nut", "-Dox +Nut", "+Dox +Nut"])),
-        yaxis=("Median exclusive pairwise distance (nm)", (150,450)))
-dotplot!(nutdoxgroups, p53_mdm2_median, mode = :density, label="", marker=(3, repeat([:orange, :darkblue, :darkred], inner=10), stroke(0)))
+        yaxis=("Median exclusive pairwise distance (nm)", (0,500)))
+dotplot!(nutdoxgroups, p53_mdm2_median, mode = :density, label="", marker=(8, repeat([:orange, :darkblue, :darkred], inner=10), stroke(0)))
 savefig(joinpath(outputdir, "p53_mdm2_median_boxplot.png"))
 
 p53_mdm2_montecarlo = [montecarlomeasurements[:,:,1,1,1] |> vec; montecarlomeasurements[:,:,2,1,1] |> vec; montecarlomeasurements[:,:,1,2,1] |> vec; montecarlomeasurements[:,:,2,2,1] |> vec]
 groupedboxplot(nutdoxgroups, p53_mdm2_montecarlo, outliers=false,
-        legend=:none,
+        label=["- Dox", "+ Dox"],
         guidefontsize=12,
-        tickfontsize=10,
-        legendfontsize=10,
+        tickfontsize=72,
+        legend=:none,
+        left_margin=20mm,
+        top_margin=5mm,
+        bottom_margin=5mm,
         seriescolor=[:white :lightgray],
-        line=(3, :gray, 1.0),
+        line=(6, 1.0),
+        size=(1024,2048),
         xaxis=("Condition", (1:4, ["-Dox -Nut", "+Dox -Nut", "-Dox +Nut", "+Dox +Nut"])),
         yaxis=("Fraction bound", (0,0.15)))
-groupeddotplot!(nutdoxgroups, p53_mdm2_montecarlo, mode = :density, label="", marker=(3, repeat([:orange, :darkblue, :darkred], inner=10), stroke(0)))
+groupeddotplot!(nutdoxgroups, p53_mdm2_montecarlo, mode = :density, label="", marker=(8, repeat([:orange, :darkblue, :darkred], inner=10), stroke(0)))
 savefig(joinpath(outputdir, "p53_mdm2_montecarlo_boxplot.png"))
 
 
@@ -345,32 +352,131 @@ montecarloresultGAPDH = anova(
 
 ### Paper figure
 
-
+import Plots.mm
 rnagroups = repeat([1,2], inner = 60)
 doxgroups = repeat([2, 1], inner = 30, outer = 2)
 p53_meg3_median = [medianmeasurements[:,:,1,1,2] |> vec; medianmeasurements[:,:,2,1,2] |> vec; medianmeasurements[:,:,1,2,2] |> vec; medianmeasurements[:,:,2,2,2] |> vec]
 groupedboxplot(rnagroups, p53_meg3_median, group = doxgroups, outliers=false,
         label=["- Dox", "+ Dox"],
         guidefontsize=12,
-        tickfontsize=10,
-        legendfontsize=10,
+        tickfontsize=72,
+        left_margin=20mm,
+        top_margin=5mm,
+        bottom_margin=5mm,
+        legend=:none,
         seriescolor=[:white :lightgray],
-        line=(3, :gray, 1.0),
+        line=(6, 1.0),
         xaxis=("RNA", (1:2, ["MEG3", "GAPDH"])),
-        size=(2048,1024),
-        yaxis=("Median exclusive pairwise distance (nm)", (150,1200)))
-groupeddotplot!(rnagroups, p53_meg3_median, group = doxgroups, mode = :density, label="", marker=(3, repeat([:orange, :darkblue, :darkred], inner=10), stroke(0)))
+        size=(1024,2048),
+        yaxis=("Median exclusive pairwise distance (nm)", (0,1200)))
+groupeddotplot!(rnagroups, p53_meg3_median, group = doxgroups, mode = :density, label="", marker=(8, repeat([:orange, :darkblue, :darkred], inner=10), stroke(0)))
 savefig(joinpath(outputdir, "p53_meg3_median_boxplot.png"))
 
 p53_meg3_montecarlo = [montecarlomeasurements[:,:,1,1,2] |> vec; montecarlomeasurements[:,:,2,1,2] |> vec; montecarlomeasurements[:,:,1,2,2] |> vec; montecarlomeasurements[:,:,2,2,2] |> vec]
 groupedboxplot(rnagroups, p53_meg3_montecarlo, group = doxgroups, outliers=false,
         label=["- Dox", "+ Dox"],
         guidefontsize=12,
-        tickfontsize=10,
-        legendfontsize=10,
+        tickfontsize=72,
+        legend=:none,
+        left_margin=20mm,
+        top_margin=5mm,
+        bottom_margin=5mm,
         seriescolor=[:white :lightgray],
-        line=(3, :gray, 1.0),
+        line=(6, 1.0),
+        size=(1024,2048),
         xaxis=("RNA", (1:2, ["MEG3", "GAPDH"])),
         yaxis=("Fraction bound", (0,0.2)))
-groupeddotplot!(rnagroups, p53_meg3_montecarlo, group = doxgroups, mode = :density, label="", marker=(3, repeat([:orange, :darkblue, :darkred], inner=10), stroke(0)))
+groupeddotplot!(rnagroups, p53_meg3_montecarlo, group = doxgroups, mode = :density, label="", marker=(8, repeat([:orange, :darkblue, :darkred], inner=10), stroke(0)))
 savefig(joinpath(outputdir, "p53_meg3_montecarlo_boxplot.png"))
+
+# localization plots of example cells
+# Exp 3 (p53-MEG3)
+# A2
+insetx, insety = [11000, 15096], [19000, 23096]
+localizationsplot_forprint(experimentresults[2][1][1][2], insetbox = [insetx, insety])
+savefig(joinpath(outputdir, "3 - A2 dSTORM points.png"))
+localizationsinsetplot_forprint(experimentresults[2][1][1][2], insetx, insety)
+savefig(joinpath(outputdir, "3 - A2 dSTORM points 10x.png"))
+moleculesinsetplot_forprint(experimentresults[2][1][1][2], insetx, insety)
+savefig(joinpath(outputdir, "3 - A2 dSTORM molecules 10x.png"))
+insetplot_forprint(experimentresults[2][1][1][2], insetx, insety)
+savefig(joinpath(outputdir, "3 - A2 dSTORM 10x.png"))
+
+# B2
+insetx, insety = [17000, 21096], [18000, 22096]
+localizationsplot_forprint(experimentresults[2][1][2][2], insetbox = [insetx, insety])
+savefig(joinpath(outputdir, "3 - B2 dSTORM points.png"))
+localizationsinsetplot_forprint(experimentresults[2][1][2][2], insetx, insety)
+savefig(joinpath(outputdir, "3 - B2 dSTORM points 10x.png"))
+moleculesinsetplot_forprint(experimentresults[2][1][2][2], insetx, insety)
+savefig(joinpath(outputdir, "3 - B2 dSTORM molecules 10x.png"))
+insetplot_forprint(experimentresults[2][1][2][2], insetx, insety)
+savefig(joinpath(outputdir, "3 - B2 dSTORM 10x.png"))
+
+# C10
+insetx, insety = [7500, 11596], [20000, 24096]
+localizationsplot_forprint(experimentresults[2][1][3][10], insetbox = [insetx, insety])
+savefig(joinpath(outputdir, "3 - C10 dSTORM points.png"))
+localizationsinsetplot_forprint(experimentresults[2][1][3][10], insetx, insety)
+savefig(joinpath(outputdir, "3 - C10 dSTORM points 10x.png"))
+moleculesinsetplot_forprint(experimentresults[2][1][3][10], insetx, insety)
+savefig(joinpath(outputdir, "3 - C10 dSTORM molecules 10x.png"))
+insetplot_forprint(experimentresults[2][1][3][10], insetx, insety)
+savefig(joinpath(outputdir, "3 - C10 dSTORM 10x.png"))
+
+
+# D10
+insetx, insety = [7500, 11596], [20000, 24096]
+localizationsplot_forprint(experimentresults[2][1][4][10], insetbox = [insetx, insety])
+savefig(joinpath(outputdir, "3 - D10 dSTORM points.png"))
+localizationsinsetplot_forprint(experimentresults[2][1][4][10], insetx, insety)
+savefig(joinpath(outputdir, "3 - D10 dSTORM points 10x.png"))
+moleculesinsetplot_forprint(experimentresults[2][1][4][10], insetx, insety)
+savefig(joinpath(outputdir, "3 - D10 dSTORM molecules 10x.png"))
+insetplot_forprint(experimentresults[2][1][4][10], insetx, insety)
+savefig(joinpath(outputdir, "3 - D10 dSTORM 10x.png"))
+
+# Exp 2 (p53-MDM2)
+# R3 A8
+insetx, insety = [14000, 18096], [28000, 32096]
+localizationsplot_forprint(experimentresults[1][3][1][8], insetbox = [insetx, insety])
+savefig(joinpath(outputdir, "2 - A8 dSTORM points.png"))
+localizationsinsetplot_forprint(experimentresults[1][3][1][8], insetx, insety)
+savefig(joinpath(outputdir, "2 - A8 dSTORM points 10x.png"))
+moleculesinsetplot_forprint(experimentresults[1][3][1][8], insetx, insety)
+savefig(joinpath(outputdir, "2 - A8 dSTORM molecules 10x.png"))
+insetplot_forprint(experimentresults[1][3][1][8], insetx, insety)
+savefig(joinpath(outputdir, "2 - A8 dSTORM 10x.png"))
+
+# R3 B5
+insetx, insety = [14000, 18096], [20000, 24096]
+localizationsplot_forprint(experimentresults[1][3][2][5], insetbox = [insetx, insety])
+savefig(joinpath(outputdir, "2 - B5 dSTORM points.png"))
+localizationsinsetplot_forprint(experimentresults[1][3][2][5], insetx, insety)
+savefig(joinpath(outputdir, "2 - B5 dSTORM points 10x.png"))
+moleculesinsetplot_forprint(experimentresults[1][3][2][5], insetx, insety)
+savefig(joinpath(outputdir, "2 - B5 dSTORM molecules 10x.png"))
+insetplot_forprint(experimentresults[1][3][2][5], insetx, insety)
+savefig(joinpath(outputdir, "2 - B5 dSTORM 10x.png"))
+
+# R2 C9
+insetx, insety = [15000, 19096], [20000, 24096]
+localizationsplot_forprint(experimentresults[1][2][3][9], insetbox = [insetx, insety])
+savefig(joinpath(outputdir, "2 - C9 dSTORM points.png"))
+localizationsinsetplot_forprint(experimentresults[1][2][3][9], insetx, insety)
+savefig(joinpath(outputdir, "2 - C9 dSTORM points 10x.png"))
+moleculesinsetplot_forprint(experimentresults[1][2][3][9], insetx, insety)
+savefig(joinpath(outputdir, "2 - C9 dSTORM molecules 10x.png"))
+insetplot_forprint(experimentresults[1][2][3][9], insetx, insety)
+savefig(joinpath(outputdir, "2 - C9 dSTORM 10x.png"))
+
+# R2 D4
+insetx, insety = [21500, 25596], [12500, 16596]
+localizationsplot_forprint(experimentresults[1][2][4][4], insetbox = [insetx, insety])
+savefig(joinpath(outputdir, "2 - D4 dSTORM points.png"))
+localizationsinsetplot_forprint(experimentresults[1][2][4][4], insetx, insety)
+savefig(joinpath(outputdir, "2 - D4 dSTORM points 10x.png"))
+moleculesinsetplot_forprint(experimentresults[1][2][4][4], insetx, insety)
+savefig(joinpath(outputdir, "2 - D4 dSTORM molecules 10x.png"))
+insetplot_forprint(experimentresults[1][2][4][4], insetx, insety)
+savefig(joinpath(outputdir, "2 - D4 dSTORM 10x.png"))
